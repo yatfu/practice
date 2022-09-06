9f6faf5c72281b581fae66f0b9ae663710cd9d87
yatfu <55002354+yatfu@users.noreply.github.com>
2022-09-06 13:49:40 -1000

    transferred files. remove boilerplate

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+const BookList = () => {
+  return (
+    <section>
+      this is a booklist
+    </section>
+  )
+}
+
+ReactDOM.render(<BookList />, document.getElementById('root'))
