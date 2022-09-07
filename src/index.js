dd85895633ca99f96cddf009ea7913d5381d6555
yatfu <55002354+yatfu@users.noreply.github.com>
2022-09-06 14:33:57 -1000

    hardcode book example

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 
-const BookList = () => {
+const BookList = () => { // contains books
   return (
     <section>
-      this is a booklist
+      <Book />
     </section>
   )
 }
 
-ReactDOM.render(<BookList />, document.getElementById('root'))
+const Book = () => {
+  return (
+    <section>
+      <Image />
+      <Title />
+    </section>
+  )
+}
+
+const Title = () => <h1>Carrie Soto Is Back: A Novel </h1>
+
+const Image = () => (
+  <img src='https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T1/images/I/51W5Kgv0MgL._AC_SX184_.jpg' alt='img'>title</img>
+)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<BookList />, document.getElementById('root'))
