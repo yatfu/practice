d51da9e9b01a2dd37ebfbbe25973c1e86610dfd7
yatfu <55002354+yatfu@users.noreply.github.com>
2022-09-08 02:09:39 -1000

    fixed deprecated issue, added boiler css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 
 const BookList = () => { // contains books
   return (
@@ -13,16 +13,19 @@ const Book = () => {
   return (
     <section>
       <Image />
-      <Title />
+      <h2><Title /></h2>
+      <h1>By: {<Author />}</h1>
     </section>
   )
 }
 
-const Title = () => <h1>Carrie Soto Is Back: A Novel </h1>
+const Title = () => 'Carrie Soto Is Back: A Novel'
 
 const Image = () => (
-  <img src='https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T1/images/I/51W5Kgv0MgL._AC_SX184_.jpg' alt='img'>title</img>
+  <img src='https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T1/images/I/51W5Kgv0MgL._AC_SX184_.jpg' alt='img'/>
 )
 
+const Author = () => 'Taylor Jenkins Reid'
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<BookList />, document.getElementById('root'))
+root.render(<BookList />)
