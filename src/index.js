d221c7df29c232498263b262ae33c3e6e95f2dec
yatfu <55002354+yatfu@users.noreply.github.com>
2022-09-11 11:09:29 -1000

    refactor
    created list for books and mapped

--- src/index.js
+++ src/index.js
@@ -4,32 +4,35 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 // VARS SETUP
+const books = [
+  {
+    image: 'https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/A1Bw-ZraGJL._AC_US218_..jpg',
+    title: 'Carrie Soto Is Back: A Novel',
+    author: 'Taylor Jenkins Reid'
+  },
+  {
+    image: 'https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/81B3NH2-s8L._AC_US218_..jpg',
+    title: 'One True Loves: A Novel',
+    author: 'Taylor Jenkins Reid' 
+  }
+]
 
-const firstBook = {
-  image: 'https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T1/images/I/51W5Kgv0MgL._AC_SX184_.jpg',
-  title: 'Carrie Soto Is Back: A Novel',
-  author: 'Taylor Jenkins Reid'
-}
 
-const secondBook = {
-  image: 'https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/41P1TdVaj+L.jpg',
-  title: 'One True Loves: A Novel',
-  author: 'Taylor Jenkins Reid' 
-}
+//const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
 
 const BookList = () => { // contains books
   return (
     <section className='booklist'>
-      <Book image={firstBook.image} title={firstBook.title} author={firstBook.author}/>
-      <Book image={secondBook.image} title={secondBook.title} author={secondBook.author}>lorem</Book>
-
+      {books.map((book) => {
+        return <Book book={book}/>
+      })}
     </section>
   )
 }
 // PROPS: image, title, author
 // note: children prop accesses in between of html
 const Book = (props) => {
-  const {image, title, author} = props
+  const {image, title, author} = props.book
   return (
     <section className='book'>
       <img src={image} alt='img'/>
